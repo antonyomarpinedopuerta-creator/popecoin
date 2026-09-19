0923248488d11bd78d86b191f96022f2355d76a2
Antony Pinedo <antonyomarpinedopuerta@gmail.com>
2026-09-19 10:31:53 -0500

    Update PAPA metadata URI reference

--- scripts/create-metadata.ts
+++ scripts/create-metadata.ts
@@ -16,7 +16,7 @@ const RPC = "https://api.devnet.solana.com";
 const MINT = publicKey("ANNSmx2Jww4HUukAvxBRSZeTqzcuqPQTiewSjxx7tgnw");
 
 const URI =
-  "https://raw.githubusercontent.com/antonyomarpinedopuerta-creator/popecoin/6bcc683/metadata/metadata.json";
+  "https://raw.githubusercontent.com/antonyomarpinedopuerta-creator/popecoin/master/metadata/metadata.json";
 
 async function main() {
   const umi = createUmi(RPC).use(mplTokenMetadata());
